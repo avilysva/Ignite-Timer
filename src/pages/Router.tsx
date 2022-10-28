f2bee5cf69fe57cd0d52844da253c6fea1bda23a
Avily Silva <avily.silva@meritum.online>
2022-10-28 11:23:01 -0300

    [added] outlet react router dom feature

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -2,12 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Home } from './Home';
 import { History } from './History';
+import { DefaultLayout } from '../layouts/DefaultLayout';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   );
 }

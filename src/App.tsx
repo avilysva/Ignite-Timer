cf08730fab66289e8a87ce9d362c72765a952c0f
Avily Silva <avily.silva@meritum.online>
2022-10-28 11:11:17 -0300

    [added] react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Router } from './pages/Router';
 
 import { defaultTheme } from './themes/default';
 import { GlobalStyles } from './themes/global';
@@ -6,7 +8,9 @@ import { GlobalStyles } from './themes/global';
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Ignite Timer</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   );
